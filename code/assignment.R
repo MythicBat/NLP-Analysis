6f6ac4337ea5c41042ee87ce30e0320c5604bf69
Alin Merchant <alinmerchant5@gmail.com>
2026-05-30 15:00:24 +1000

    initial commit and creating dataset

---
+++ code/assignment.R
@@ -0,0 +1,62 @@
+install.packages(c("tidyverse", "tm", "SnowballC", "proxy", "cluster", "syuzhet", "igraph", "ggraph", "tidygraph", "ggplot2"))
+
+library(tidyverse)
+library(tm)
+library(SnowballC)
+library(proxy)  
+library(cluster)
+library(syuzhet)
+library(igraph)
+library(ggraph)
+library(tidygraph)
+library(ggplot2)
+
+# Load the dataset
+corpus_df <- read.csv("data/corpus.csv", stringsAsFactors = FALSE)
+
+# Check structure
+str(corpus_df)
+head(corpus_df)
+
+table(corpus_df$genre)
+
+# Check word counts
+corpus_df$word_count <- sapply(strsplit(corpus_df$text, "\\s+"), length)
+
+summary(corpus_df$word_count)
+
+# QUESTION 3: Text Processing and Document-Term Matrix
+
+# Create text corpus from the text column
+text_corpus <- VCorpus(VectorSource(corpus_df$text))
+
+# Basic text cleaning
+text_corpus <- tm_map(text_corpus, content_transformer(tolower))
+text_corpus <- tm_map(text_corpus, removePunctuation)
+text_corpus <- tm_map(text_corpus, removeNumbers)
+text_corpus <- tm_map(text_corpus, removeWords, stopwords("english"))
+text_corpus <- tm_map(text_corpus, stripWhitespace)
+
+# stemming
+text_corpus <- tm_map(text_corpus, stemDocument)
+
+# Create initial DTM
+dtm <- DocumentTermMatrix(text_corpus)
+
+dtm
+
+# Trial-and-error sparse term removal
+# Increase/decrease this value unitl around 25 terms remain
+dtm_sparse <- removeSparseTerms(dtm, 0.55)
+
+dtm_sparse
+
+# Convert dtm to matrix
+dtm_matrix <- as.matrix(dtm_sparse)
+
+# Add document IDs as row names
+rownames(dtm_matrix) <- corpus_df$doc_id
+
+# View final tokens
+colnames(dtm_matrix)
+
